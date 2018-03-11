8f965994b530211581001bdc69a0098adf7263dc
qvphong <quachvanphong125@gmail.com>
2018-03-12 01:26:33 +0700

    sas

--- src/main/java/coin/SignerBtc/App.java
+++ src/main/java/coin/SignerBtc/App.java
@@ -2,16 +2,23 @@ package coin.SignerBtc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class);
-    }
+public class App {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class);
+	}
 }
