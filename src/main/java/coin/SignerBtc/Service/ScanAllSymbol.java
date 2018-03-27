cacd843d7798763f6cb5b6d074c28ccc208f2783
qvphong <quachvanphong125@gmail.com>
2018-03-27 16:33:09 +0700

    xxzc

--- src/main/java/coin/SignerBtc/Service/ScanAllSymbol.java
+++ src/main/java/coin/SignerBtc/Service/ScanAllSymbol.java
@@ -21,82 +21,86 @@ import com.webcerebrium.binance.datatype.BinanceSymbol;
 @Service
 public class ScanAllSymbol {
 
-	private Logger logger = LoggerFactory.getLogger(ScanAllSymbol.class);
+  private Logger logger = LoggerFactory.getLogger(ScanAllSymbol.class);
 
-	public Map<String, BigDecimal> scanAll(BinanceApi api) {
-		Map<String, BigDecimal> mapPrice = null;
-		try {
-			mapPrice = api.pricesMap();
-		} catch (BinanceApiException e) {
-			logger.error("--> ERROR: Scan all price: {}", e);
-		}
-		return mapPrice;
-	}
+  public Map<String, BigDecimal> scanAll(BinanceApi api) {
+    Map<String, BigDecimal> mapPrice = null;
+    try {
+      mapPrice = api.pricesMap();
+    } catch (BinanceApiException e) {
+      logger.error("--> ERROR: Scan all price: {}", e);
+    }
+    return mapPrice;
+  }
 
-	public Set<String> filterPrice(Map<String, BigDecimal> maps, String symbol) {
-		Set<String> symbols = new HashSet<String>();
-		for (String key : maps.keySet()) {
-			if (key.contains(symbol) && maps.get(key).compareTo(new BigDecimal("0.002")) < 0) {
-				symbols.add(key);
-			}
-		}
-		return symbols;
-	}
+  public Set<String> filterPrice(Map<String, BigDecimal> maps, String symbol) {
+    Set<String> symbols = new HashSet<String>();
+    for (String key : maps.keySet()) {
+      if (key.contains(symbol) && maps.get(key).compareTo(new BigDecimal("0.002")) < 0) {
+        symbols.add(key);
+      }
+    }
+    return symbols;
+  }
 
-	public Boolean filterName(String name, String symbolCompare) {
-		if (name.contains(symbolCompare)) {
-			return true;
-		}
-		return false;
-	}
+  public Boolean filterName(String name, String symbolCompare) {
+    if (name.contains(symbolCompare)) {
+      return true;
+    }
+    return false;
+  }
 
-	public Boolean IsUpTrend(LinkedList<BigDecimal> lstNew) {
-		BigDecimal elementBefore = null;
-		Iterator<BigDecimal> itr = lstNew.iterator();
-		while (itr.hasNext()) {
-			BigDecimal val = itr.next();
-			if (elementBefore != null) {
-				if (elementBefore.compareTo(val) > 0) {
-					return false;
-				} else {
-					elementBefore = val;
-				}
-			} else {
-				elementBefore = val;
-			}
-		}
-		return true;
-	}
+  public Boolean IsUpTrend(LinkedList<BigDecimal> lstNew) {
+    BigDecimal elementBefore = null;
+    Iterator<BigDecimal> itr = lstNew.iterator();
+    while (itr.hasNext()) {
+      BigDecimal val = itr.next();
+      if (elementBefore != null) {
+        if (elementBefore.compareTo(val) > 0) {
+          return false;
+        } else {
+          elementBefore = val;
+        }
+      } else {
+        elementBefore = val;
+      }
+    }
+    return true;
+  }
 
-	public List<BinanceCandlestick> getOldCandltick(BinanceSymbol symbol) throws BinanceApiException {
-		BinanceApi api = new BinanceApi();
-		MathContext mc = new MathContext(2, RoundingMode.HALF_UP);
-		List<BinanceCandlestick> kLine = api.klines(symbol, BinanceInterval.FIFTEEN_MIN, 10, null);
-		BinanceCandlestick candl1 = kLine.get(kLine.size() - 3);
-		BinanceCandlestick candl2 = kLine.get(kLine.size() - 2);
-		if (candl1.getVolume().compareTo(new BigDecimal(0)) != 0 && (candl2.getVolume().divide(candl1.getVolume(), mc)).compareTo(new BigDecimal(2)) >= 0) {
-			if (candl1.getClose().compareTo(candl1.getOpen()) < 0 || candl2.getClose().compareTo(candl2.getOpen()) < 0) {
-				return null;
-			}
-			BigDecimal sub1 = candl1.getClose().subtract(candl1.getOpen());
-			BigDecimal mid1 = candl1.getClose().subtract(candl1.getOpen()).divide(new BigDecimal("2"), mc);
-			BigDecimal sub2 = candl2.getClose().subtract(candl2.getOpen());
-			if (sub1.compareTo(new BigDecimal(0)) != 0 && sub2.divide(sub1, mc).intValue() >= 2 && candl2.getOpen().compareTo(mid1) > 0) {
-				if (chechMinOk(kLine)) {
-					return kLine;
-				}
-			}
-		}
-		return null;
-	}
+  public List<BinanceCandlestick> getOldCandltick(BinanceSymbol symbol) throws BinanceApiException {
+    BinanceApi api = new BinanceApi();
+    MathContext mc = new MathContext(2, RoundingMode.HALF_UP);
+    List<BinanceCandlestick> kLine = api.klines(symbol, BinanceInterval.FIFTEEN_MIN, 7, null);
+    if (kLine.isEmpty() || kLine.size() < 3) {
+      logger.info("Symbol: "+ symbol + " data size: " + kLine.size());
+      return null;
+    }
+    BinanceCandlestick candl1 = kLine.get(kLine.size() - 3);
+    BinanceCandlestick candl2 = kLine.get(kLine.size() - 2);
+    if (candl1.getVolume().compareTo(new BigDecimal(0)) != 0 && (candl2.getVolume().divide(candl1.getVolume(), mc)).compareTo(new BigDecimal(2)) >= 0) {
+      if (candl1.getClose().compareTo(candl1.getOpen()) < 0 || candl2.getClose().compareTo(candl2.getOpen()) < 0) {
+        return null;
+      }
+      BigDecimal sub1 = candl1.getClose().subtract(candl1.getOpen());
+      BigDecimal mid1 = candl1.getClose().subtract(candl1.getOpen()).divide(new BigDecimal("2"), mc);
+      BigDecimal sub2 = candl2.getClose().subtract(candl2.getOpen());
+      if (sub1.compareTo(new BigDecimal(0)) != 0 && sub2.divide(sub1, mc).intValue() >= 2 && candl2.getOpen().compareTo(mid1) > 0) {
+        if (chechMinOk(kLine)) {
+          return kLine;
+        }
+      }
+    }
+    return null;
+  }
 
-	public Boolean chechMinOk(List<BinanceCandlestick> kLine) {
-		BigDecimal min = kLine.get(kLine.size() - 3).getClose();
-		for (int i = kLine.size() - 6; i >= 0; i--) {
-			if (kLine.get(i).getClose().compareTo(min) <= 0) {
-				return false;
-			}
-		}
-		return true;
-	}
+  public Boolean chechMinOk(List<BinanceCandlestick> kLine) {
+    BigDecimal min = kLine.get(kLine.size() - 3).getClose();
+    for (int i = kLine.size() - 6; i >= 0; i--) {
+      if (kLine.get(i).getClose().compareTo(min) <= 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
